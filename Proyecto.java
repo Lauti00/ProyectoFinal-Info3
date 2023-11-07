207848e75ce25411eade21b517f67bd9244cf3f1
lautaro <lautaro>
2023-11-07 18:17:19 -0300

    Cambios finales

--- Proyecto.java
+++ Proyecto.java
@@ -20,10 +20,10 @@ public class Proyecto {
                 case 1:
                     add(tree);
                     break;
-                case 2:{
+                case 2:
                     remove(tree);
                     break;
-                    }
+                    
                 case 3:
                     search(tree);
                     break;
@@ -47,68 +47,81 @@ public class Proyecto {
         int stock = 0;
         boolean flag = false;
         Scanner input = new Scanner(System.in);
-        do{
-            try{
+        
+        do {
+            try {
                 System.out.println("Ingrese el nombre del producto");
                 name = input.next();
                 name = name.toLowerCase();
-            }catch(Exception e){
-                throw new IllegalArgumentException("El nombre debe ser String");
+            } catch (Exception e) {
+                System.out.println("El nombre debe ser un String válido. Intente nuevamente.");
+                input.nextLine();  // Limpiar el búfer de entrada
+                continue;  // Volver al principio del bucle para ingresar un valor válido
             }
+    
             System.out.println("Ingrese la cantidad de stock del producto");
-            try{
-                stock = input.nextInt();
-            }catch(Exception e){
-                throw new IllegalArgumentException("El stock tiene que ser entero");
-            }
-            Producto producto= new Producto(name,stock);
             
-            if(tree.find(producto) == null){
-                Producto producto1= new Producto(name,stock);
+            try {
+                stock = input.nextInt();
+            } catch (Exception e) {
+                System.out.println("El stock debe ser un número entero. Intente nuevamente.");
+                input.nextLine();  // Limpiar el búfer de entrada
+                continue;  // Volver al principio del bucle para ingresar un valor válido
+            }
+            
+            Producto producto = new Producto(name, stock);
+    
+            if (tree.find(producto) == null) {
+                Producto producto1 = new Producto(name, stock);
                 tree.insert(producto1);
                 System.out.println("Producto cargado correctamente");
                 flag = true;
-                
-            }else{
-                System.out.println("Ya se ha ingresado un producto con ese nombre...\nDesea agregarle mas stock al producto ? \n(Presione 'y' para aceptar o 'n' para rechazar)");
+            } else {
+                System.out.println("Ya se ha ingresado un producto con ese nombre...\nDesea agregarle más stock al producto ? \n(Presione 'y' para aceptar o 'n' para rechazar)");
                 option = input.next().charAt(0);
-                System.out.println("Ingrese la cantidad que desea sumarle al stock");
-                stock = input.nextInt();
-                if(option == 'y'){
+                if (option == 'y') {
+                    System.out.println("Ingrese la cantidad que desea sumarle al stock");
+                    stock = input.nextInt();
                     System.out.println("Producto actualizado correctamente");
                     System.out.println("---------------------------------------------------");
                     System.out.println("Nombre\tStock");
                     System.out.println(tree.changeStock(producto, stock));
                     System.out.println("---------------------------------------------------");
                     flag = true;
-                }else
+                } else {
                     break;
+                }
             }
-        }while(flag==false);
-
+        } while (!flag);
     }
     
-    public static void remove(BinarySearchTree tree){
-        String name =" ";
+    public static void remove(BinarySearchTree tree) {
+        String name = " ";
         Scanner console = new Scanner(System.in);
-        System.out.println("Ingrese el nombre del producto a borrar");
-        try{
-            name = console.next();
-        }catch (Exception e){
-            throw new IllegalArgumentException("El nombre debe ser un String");
-        }
-        Producto producto = new Producto(name);
-        if(tree.find(producto) == null){
-           System.out.println("No se encontro el producto");
-            //throw new IllegalArgumentException();
-        }else{
-            tree.remove(producto);
-            System.out.println("Producto removido correctamente");
-        }
-                    
+        
+        do {
+            try {
+                System.out.println("Ingrese el nombre del producto a borrar");
+                name = console.next();
+            } catch (Exception e) {
+                System.out.println("El nombre debe ser un String válido. Intente nuevamente.");
+                console.nextLine();  // Limpiar el búfer de entrada
+                continue;  // Volver al principio del bucle para ingresar un valor válido
+            }
+    
+            Producto producto = new Producto(name);
+            if (tree.find(producto) == null) {
+                System.out.println("No se encontró el producto. Intente nuevamente.");
+            } else {
+                tree.remove(producto);
+                System.out.println("Producto removido correctamente");
+                break;  // Salir del bucle si se encontró y eliminó el producto
+            }
+        } while (true);  // Repetir hasta que se encuentre y elimine el producto
     }
     
     public static void search(BinarySearchTree tree){
+       do{
         String name = " ";
         Scanner console = new Scanner(System.in);
 
@@ -126,7 +139,9 @@ public class Proyecto {
         else{
             System.out.println("Nombre\tStock");
             System.out.println(tree.findName(producto));
-        }                            
+            break;
+        }
+    }while(true);                            
     }
 
     public static void print(BinarySearchTree tree){
